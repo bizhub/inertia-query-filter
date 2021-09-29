98201723adfd170ff39f74c5e50f1130d5526086
Lex van der Woude <lex@bizhub.co.nz>
2021-09-29 21:23:05 +1300

    Add defaults

--- src/Filterable.php
+++ src/Filterable.php
@@ -8,12 +8,13 @@ trait Filterable
      * Filter scope
      *
      * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  array  $defaults
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeFilter($query)
+    public function scopeFilter($query, $defaults = [])
     {
         $defaultClassName = '\\App\\QueryFilters\\' . class_basename($this) . 'Filter';
 
-        return (new $defaultClassName)->apply($query);
+        return (new $defaultClassName)->apply($query, $defaults);
     }
 }
